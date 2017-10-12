fdeaff569b97fd4b3899894a24df4fcb99e2088b
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:42:29 +0300

    7 desc in

--- TTZC0200/Harj7/Program.cs
+++ TTZC0200/Harj7/Program.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka kysyy käyttäjältä nimen, pituuden sentteinä ja painon kiloina.
+ *  Ohjelma laskee ja tulostaa käyttäjän ihannepainon (pituus-100). Lisäksi
+ *  ohjelma kertoo käyttäjän nykyisen painon eron kiloina verrattuna ihanne-
+ *  painoon. Ohjelman käyttöliittymä toimii seuraavasti:
+ *  
+ *  Ihannepaino
+ *  
+ *  Ohjelma laskee ihannepainosi
+ *  pituutesi perusteella.
+ *  
+ *  Anna nimesi > Mooses
+ *  Anna pituutesi sentteinä > 175
+ *  Anna osoitteesi > Peikkovuori 5 as 4
+ *  Anna painosi kiloina > 89
+ *  
+ *  
+ *  Arvoisa Mooses
+ *  Osoitteesi on Peikkovuori 5 as 4
+ *  Nykyinen painosi 89.0 kg
+ *  Ihannepainosi 75.0 kg
+ *  Erotus 14.0 kg
+ *  
+ *  
+ *  Käytä cin/cout/cin.get... olioita ohjelman
+ *  toteuttamiseen (c++ metodit, kirja sivu 92 - 99)
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
