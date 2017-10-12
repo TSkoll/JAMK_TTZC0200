1bc36ab1f6944404aa347eb7fffd071f46267eaf
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:47:55 +0300

    13 desc in

--- TTZC0200/Harj13/Program.cs
+++ TTZC0200/Harj13/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka kysyy etunimesi ja sukunimesi. Ohjelma
+ *  yhdistää nimet yhdeksi merkkijonoksi ja tulostaa ne
+ *  lopuksi naytölle.
+ *  
+ *  
+ *  a) Kayta cstring-kirjastoa (C:n merkkitaulukot)
+ *  tehdäksesi merkkijonojen yhdistämisen
+ *  b) Kayta string-kirjastoa (C++:n merkkijonot)
+ *  tehdäksesi merkkijonojen yhdistämisen  TEE, JOS ON AIKAA
+ *  
+ *  Esimerkki
+ *  Anna etunimi: Aku
+ *  Anna sukunimi: Ankka
+ *  Nimesi oli: Aku Ankka
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
