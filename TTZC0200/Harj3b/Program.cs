955856a495c1788903ab51eabdd2fd113973e4e0
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:40:06 +0300

    3b desc in

--- TTZC0200/Harj3b/Program.cs
+++ TTZC0200/Harj3b/Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ *  Muokkaa edellisestä ohjelmasta versio nimelle harj3b.cpp, jossa
+ *  nimi ja osoite tulostetaan näytälle seuraavasti:
+ *  
+ *  **********************************
+ *  * *
+ *  * Etunimi *
+ *  * Sukunimi *
+ *  * *
+ *  *-------------------------------------- *
+ *  * *
+ *  * Asemakatu 7 *
+ *  * 40520 JKL *
+ *  * *
+ *  **********************************
+ *
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
