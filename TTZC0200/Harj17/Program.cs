09e310d94fe128ca0e2e5160090c7abdb26f971b
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:52:02 +0300

    17 desc in

--- TTZC0200/Harj17/Program.cs
+++ TTZC0200/Harj17/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+ *  Muokkaa edellistä ohjelmaa siten, että edellä määriteltyä tietuetyyppiä
+ *  käytetään myäs kahden muun "koululaisen" tietojen tallentamiseen.
+ *  Näiden kahden muun koululaisen tiedot alustetaan ao. muuttujien
+ *  määrittelyn yhteydessä. Ainoastaan yhden koululaisen tiedot kysytään
+ *  käyttäjältä edellisen tehtävän tapaan.
+ *  
+ *  Tulosta kolmen koululaisen tiedot koulumatkan mukaisessa
+ *  suuruusjärjestyksessä (pienimmästä suurimpaan) näytälle
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
