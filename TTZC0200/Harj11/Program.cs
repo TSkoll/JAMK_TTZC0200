92aa16582c07f81cf31224e0ef462c9c4fb48c61
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:45:43 +0300

    11 desc in

--- TTZC0200/Harj11/Program.cs
+++ TTZC0200/Harj11/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ *  Muuta tehtävän 9 laskin-ohjelmaa niin, etta valikko nayttaa
+ *  seuraavalta:
+ *  
+ *  VALIKKO
+ *  0. Lopetus
+ *  1. Summa
+ *  2. Erotus
+ *  3. Tulo
+ *  4. Osamaara
+ *  5. Jakojaannos
+ *  6. Syota uudet luvut laskemista varten
+ *  
+ *  Laskimella on siis mahdollista käsitellä
+ *  useita lukupareja käyttäjän toiveiden
+ *  mukaisesti. Mita tapahtuu, jos luku2 on 0?
+ *  HUOM! goto -lauseen käyttä on KIELLETTY!!!!
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
