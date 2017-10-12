9d05d91f36196e644826904537f252a4923af18e
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:50:27 +0300

    15 desc in

--- TTZC0200/Harj15/Program.cs
+++ TTZC0200/Harj15/Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka kysyy henkilötietosi seuraavasti:
+ *  Anna (kaikki) etunimesi (merkkijono):
+ *  Anna kengannumero (kokonaisluku):
+ *  Anna sukunimi (merkkijono):
+ *  Anna koulumatka (reaaliluku):
+ *  Anna osoitteesi:
+ *  Anna postinumero:
+ *  
+ *  Ohjelma tulostaa tiedot seuraavasti:
+ *  sukunimi etunimet
+ *  osoite
+ *  postinumero
+ *  kengannumero ja koulumatka
+ *  
+ *  Käytä C++:n cin- ja cout-olioita ohjelman toteuttamiseen.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
