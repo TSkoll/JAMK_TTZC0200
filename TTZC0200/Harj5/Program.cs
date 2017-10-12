278be4dca7f49238b70f534d08d40dc7cee89891
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:40:48 +0300

    5 desc in

--- TTZC0200/Harj5/Program.cs
+++ TTZC0200/Harj5/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ *  Kirjoita ohjelma, joka pyytää käyttäjältä kaksi kokonaislukua.
+ *  
+ *  Ohjelma tulostaa tekstin BINGO, mikäli luvut ovat samoja.
+ *  
+ *  Muussa tapauksessa ohjelma tulostaa tekstin BONGO.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
