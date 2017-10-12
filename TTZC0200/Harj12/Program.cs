84ceacea49e2179c859aad3e87b9b3fa080cd89d
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:47:14 +0300

    12 desc in

--- TTZC0200/Harj12/Program.cs
+++ TTZC0200/Harj12/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka kysyy käyttäjältä kokonaisluvun väliltä 1-9 ja
+ *  tulostaa vastauksen perusteella seuraavan kuvion (jos vastaus on 6):
+ *  
+ *  1
+ *  22
+ *  333
+ *  4444
+ *  55555
+ *  666666
+ *  
+ *  Jokainen numero tulee tulostaa toistorakenten avulla erikseen lauseella:
+ *  cout << rivinro;
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
