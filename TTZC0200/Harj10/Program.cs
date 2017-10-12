72e0766ffd76a5b6af19e6a60d5861a5f0744f11
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:44:50 +0300

    10 desc in

--- TTZC0200/Harj10/Program.cs
+++ TTZC0200/Harj10/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka laskee syötetyistä kokonaisluvuista sekä positiivisten
+ *  että negatiivisten kokonaislukujen osuuden. Lukujen syöttä lopetetaan
+ *  syöttämällä luku 0.
+ *  
+ *  Tulostus:
+ *  Syötit kokonaislukuja seuraavasti:
+ *  ----------------------------------
+ *  Negatiiviset 7 kpl 70.00%
+ *  Positiiviset 3 kpl 30.00%
+ *  Yhteensä 10 kpl 100.00%
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
