9858726194c49d18aba1fd8c632916323c2adf5b
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:44:05 +0300

    9 desc in

--- TTZC0200/Harj9/Program.cs
+++ TTZC0200/Harj9/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka toimii laskimena.
+ *  Käyttäjältä kysytään kaksi kokonaislukua,
+ *  jonka jälkeen ohjelma antaa valikon
+ *  laskutoimituksista. Valikko nayttaa
+ *  seuraavalta:
+ *  
+ *  VALIKKO
+ *  1. Summa
+ *  2. Erotus
+ *  3. Tulo
+ *  4. Osamaara
+ *  5. Jakojaannos
+ *  
+ *  Lopuksi ohjelma tulostaa valitun tuloksen naytölle.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
