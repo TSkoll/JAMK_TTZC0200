37b4ab2b369fb3ed6ba6e3292b4b6d11dbe849af
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:59:09 +0300

    22 desc in

--- TTZC0200/Harj22/Program.cs
+++ TTZC0200/Harj22/Program.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka toimii henkilörekisterinä (max 10).
+ *  Ohjelma antaa käyttäjälle seuraavan valikon:
+ *  
+ *  VALIKKO
+ *  0 Lopeta
+ *  1 Lisaa henkilo
+ *  2 Nayta kaikki henkilot
+ *  
+ *  Tallenna henkilöiden tiedot tietuetaulukkoon.
+ *  Tallennettavia tietoja ovat
+ *  etunimi (merkkijono)
+ *  koulumatka (liukuluku)
+ *  hatun koko (kokonaisluku)
+ *  
+ *  Toteuta ensin koko ohjelma pääohjelmana. 
+ *  
+ *  Tämän jälkeen lisää seuraavat aliohjelmat:
+ *  a) int Valikko(void);
+ *  
+ *  b) void TulostaHenkilo(TIEDOT );
+ *  c) void TulostaKaikkiHenkilot(TIEDOT [], int lkm);
+ *  d) void LisaaHenkilo(TIEDOT [], int *lkm);
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
