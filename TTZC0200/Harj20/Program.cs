07b22b13e7f5f2675252f0b1e343a4dd9bbeb6cb
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:57:27 +0300

    20 desc in

--- TTZC0200/Harj20/Program.cs
+++ TTZC0200/Harj20/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ *  Kuten harjoitus 19, mutta toteuta kayttaen taulukkoa ja osoitinta
+ *  tiedon käsittelyyn
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
