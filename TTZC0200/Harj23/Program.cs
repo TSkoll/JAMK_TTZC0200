0f60c8f76f3b135dc2dc6378b8abec53975415ca
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 22:01:24 +0300

    23 desc in

--- TTZC0200/Harj23/Program.cs
+++ TTZC0200/Harj23/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+ *  Jaa aiemmin esitetty henkilörekisteri-ohjelma omiin "osa-projekteihin" ja
+ *  otsikkotiedostoihin. Laita pääohjelma omaan tiedostoon (paa.cpp),
+ *  aliohjelmat omaan tiedostoon (ali.cpp) ja
+ *  määritelmät omaan otsikkotiedostoon (maarittely.h).
+ *  
+ *  Projekti tulee koostaa toimivaksi Visual Studio-
+ *  ympäristösssä.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
