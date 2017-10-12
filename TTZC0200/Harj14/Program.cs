c96f2ba6dd4dde4f768af930647099eefa677163
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:49:14 +0300

    14 desc in

--- TTZC0200/Harj14/Program.cs
+++ TTZC0200/Harj14/Program.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/*
+ *  TäSSä TEHTÄVÄSSÄ ON KÄYTETTÄVÄ C-kielen merkkitaulukoita! Taulukon määrittely on siis muotoa 
+ *  
+ *  char mjono[xx];
+ *  
+ *  Tee ohjelma, joka kysyy käyttäjältä 17 merkkiä
+ *  pitkän merkkijonon (ei välilyäntejä).
+ *  a) Ohjelma tulostaa merkkijonon käänteisessä järjestyksessä näytälle
+ *  (tulostus tyyppiä "cout << mjono1[4];" jne)
+ *  b) Ohjelma kääntää annetun merkkijonon toiseen
+ *  merkkijonoon ja tulostaa sen näytälle
+ *  (tulostus tyyppiä "cout << mjono2;")
+ *  c) merkkijono voi olla kuinka pitkä
+ *  tahansa (max 100 kirjainta).
+ *  Syätetyn merkkijonon pituutta ei saa
+ *  laskea millään kirjastofunktiolla
+ *  (esim. lenght tms.) Ohjelma
+ *  tutkii onko annettu merkkijono
+ *  palindromi ja ilmoittaa sen käyttäjälle.
+ *  Välilyännit jonossa ovat sallittuja.
+ *  
+ *  Käyttäjä syöttää jonon: ABC_Kissa_kavelee
+ *  ohjelma tulostaa: eelevak_assiK_CBA
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
