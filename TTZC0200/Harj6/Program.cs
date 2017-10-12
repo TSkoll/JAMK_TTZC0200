c116a3824721e74816dfabeddd9fa63f27a28ee5
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:41:36 +0300

    6 desc in

--- TTZC0200/Harj6/Program.cs
+++ TTZC0200/Harj6/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+ *  Kirjoita ohjelma, joka kysyy käyttäjältä lompakossa olevan rahamäärän
+ *  ja lihapiirakan hinnan.
+ *  
+ *  Ohjelma tutkii, onko käyttäjällä varaa ostaa lihapiirakka
+ *  Jos on, ohjelma ilmoittaa, kuinka paljon lompakkoon jää vielä rahaa.
+ *  Muuten ohjelma kehottaa käyttäjää paastoamaan.
+ *  
+ *  Käytä vain kahta muuttujaa. älä vähennä lompakon sisältää, ellet voi
+ *  ostaa lihapiirakkaa.
+___________________
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
