323a0dde2bdb2ea0e7b824e63ebbede284c19e6c
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:51:14 +0300

    16 desc in

--- TTZC0200/Harj16/Program.cs
+++ TTZC0200/Harj16/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka kysyy henkilotietosi ja tallentaa ne tietueeseen.
+ *  Tietueeseen talletetaan seuraavat tiedot:
+ *  etunimi (merkkijono; C:n merkkitaulukko)
+ *  sukunimi (merkkijono; C:n merkkitaulukko)
+ *  koulumatka (reaaliluku)
+ *  osoite (merkkijono; C:n merkkitaulukko)
+ *  postinumero (merkkijono; C:n merkkitaulukko)
+ *  kengannumero (kokonaisluku)
+ *  
+ *  Ohjelma tulostaa lopuksi tietueen tiedot naytölle.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
