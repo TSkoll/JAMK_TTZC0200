0153a76a81127773607a9702f25eb8849759595b
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:58:10 +0300

    21 desc in

--- TTZC0200/Harj21/Program.cs
+++ TTZC0200/Harj21/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ *  Muunna harjoitus 11 niin, etta käytät seuraavia aliohjelmia:
+ *  
+ *  int KysyValinta(void);
+ *  
+ *  -> KysyValinta paluttaa käyttäjän antaman valintanumeron (0-6)
+ *  
+ *  double Summa(float luku1, int luku2);
+ *  
+ *  -> Summa laskee yhteen annetut tiedot ja palauttaa summan pääohjelmaan
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
