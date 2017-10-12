52ea62592ed92ec35bac23a6b1e5be42b4024355
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 22:01:30 +0300

    25 desc in

--- TTZC0200/Harj25/Program.cs
+++ TTZC0200/Harj25/Program.cs
@@ -1,4 +1,36 @@
-﻿using System;
+﻿/*
+ *  Muuta harjoitusta 22 niin, etta valikko nayttaa seuraavalta:
+ *  
+ *  VALIKKO
+ *  0 Lopeta
+ *  1 Lisaa henkilo
+ *  2 Nayta henkilo
+ *  3 Nayta kaikki henkilot
+ *  4 Poista henkilon tiedot
+ *  5 Tallenna tiedot tiedostoon
+ *  6 Hae tiedot tiedostosta
+ *  
+ *  
+ *  ja toteuta valikossa näkyvät toiminnot.
+ *  
+ *  Valitse haluamasi tallennusformaatti.
+ * 
+ * ---------------------------------------------------------------
+ * 
+ *  Muuta harjoituksen 24 toiminnallisuutta niin, etta
+ *  varaat henkilötiedoille tilan dynaamisesti (max 10 hlo).
+ *  Muuta valikko alla olevan esimerkin mukaiseksi ja toteuta valikossa
+ *  näkyvät toiminnot.
+ *  
+ *  VALIKKO
+ *  0 Lopeta
+ *  1 Lisaa henkilo
+ *  2 Nayta henkilo
+ *  3 Nayta kaikki henkilot
+ *  4 Poista henkilon tiedot
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
