f1b171fcd22e51ce30c75d0d25a0b49d64c6ea14
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:55:03 +0300

    18 desc in

--- TTZC0200/Harj18/Program.cs
+++ TTZC0200/Harj18/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+ *  Tee ohjelma, joka kysyy viiden koiran nimet ja iät.
+ *  Tiedot tallennetaan tietuetaulukkoon.
+ *  a) järjestä tiedot ikä -kentän mukaan suuruusjärjestykseen
+ *  (pienimmästä suurimpaan). Ohjelma tulostaa lopuksi
+ *  jarjestetyn taulukon näytälle.
+ *  b) järjestä tiedot nimi -kentän mukaan aakkosjärjestyksessä
+ *  Ohjelma tulostaa lopuksi jarjestetyn taulukon näytolle.
+ *  
+ *  Lajittelu tulee toteuttaa siten, että se toimisi samoin
+ *  myäs 50 tai 5000 koiran tapauksssa.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
