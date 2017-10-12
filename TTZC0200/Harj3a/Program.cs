1765c9930f6a2599d133f97e8d955fb96486c2b2
DoubleKilled <ronkainen.eero@gmail.com>
2017-10-12 21:38:51 +0300

    3a desc in

--- TTZC0200/Harj3a/Program.cs
+++ TTZC0200/Harj3a/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+ *  #include <iostream>  
+ *  using namespace std;
+ *  void main (void)
+ * 
+ *  {
+ *      cout << "Hei maailma!";
+ *  }
+ *  
+ *  Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ *  ja osoitteesi naytolle allekkain.
+ *  HUOM! Nimeä tiedosto harj3a.cpp nimelle!
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
