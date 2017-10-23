3ad16c4d99701b01818f17c14f01826c8bc08fe3
L5047 <L5047@student.jamk.fi>
2017-10-23 15:23:06 +0300

    Harj 16 done

--- TTZC0200/Harj16/Program.cs
+++ TTZC0200/Harj16/Program.cs
@@ -23,6 +23,38 @@ namespace Harj16
     {
         static void Main(string[] args)
         {
+            Tiedot henk = new Tiedot();
+
+            Console.WriteLine("Anna etunimesi");
+            henk.Nimi = Console.ReadLine();
+
+            Console.WriteLine("Anna kengännumerosi");
+            henk.KNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Anna sukunimesi");
+            henk.SNimi = Console.ReadLine();
+
+            Console.WriteLine("Anna koulumatkasi pituus");
+            henk.KMatka = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Anna osoitteesi");
+            henk.Osoite = Console.ReadLine();
+
+            Console.WriteLine("Anna postinumerosi");
+            henk.PNum = Console.ReadLine();
+
+            Console.WriteLine($"\n{henk.Nimi} {henk.SNimi}\n{henk.Osoite}\n{henk.PNum}\n{henk.KNum} {henk.KMatka}");
+            Console.ReadKey();
         }
+    }
+
+    public struct Tiedot
+    {
+        public string Nimi { get; set; }
+        public int KNum { get; set; }
+        public string SNimi { get; set; }
+        public float KMatka { get; set; }
+        public string Osoite { get; set; }
+        public string PNum { get; set; }
     }
 }
