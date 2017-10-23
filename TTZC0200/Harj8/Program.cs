70a35d26b38817951be681fa2ffcfb9b7a2e2244
L5047 <L5047@student.jamk.fi>
2017-10-23 13:42:56 +0300

    Harj 8 done

--- TTZC0200/Harj8/Program.cs
+++ TTZC0200/Harj8/Program.cs
@@ -18,6 +18,35 @@ namespace Harj8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Anna kolme arvoa välilyönnillä erottaen ne.");
+            string[] arvot = Console.ReadLine().Split(' ');
+
+            float luku1 = Convert.ToSingle(arvot[0]);
+            float luku2 = Convert.ToSingle(arvot[1]);
+            float luku3 = Convert.ToSingle(arvot[2]);
+
+            if (luku1 < luku2)
+            {
+                if (luku2 < luku3)
+                {
+                    Console.WriteLine($"{luku1} < {luku2} < {luku3}");
+                }
+                else
+                {
+                    Console.WriteLine($"{luku1} < {luku3} < {luku2}");
+                }
+            }
+            else
+            {
+                if (luku1 < luku3)
+                {
+                    Console.WriteLine($"{luku2} < {luku1} < {luku3}");
+                }
+                else
+                {
+                    Console.WriteLine($"{luku2} < {luku3} < {luku1}");
+                }
+            }
         }
     }
 }
