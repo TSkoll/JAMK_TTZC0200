1ece734a8da35a91d10f53bd144c66384f5a880a
L5047 <L5047@student.jamk.fi>
2017-10-23 14:25:32 +0300

    Harj 9 done

--- TTZC0200/Harj9/Program.cs
+++ TTZC0200/Harj9/Program.cs
@@ -27,6 +27,47 @@ namespace Harj9
     {
         static void Main(string[] args)
         {
+            float lk1 = KysyLuku("Anna ensimmäinen luku");
+            float lk2 = KysyLuku("Anna toinen luku");
+
+            try
+            {
+                float tulos = Valikko(lk1, lk2);
+                Console.WriteLine(tulos);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey(true);
+        }
+
+        static float Valikko(float lk1, float lk2)
+        {
+            byte valinta = Convert.ToByte(Console.ReadLine());
+            switch (valinta)
+            {
+                case 1:
+                    return lk1 + lk2;
+                case 2:
+                    return lk1 - lk2;
+                case 3:
+                    return lk1 * lk2;
+                case 4:
+                    if (lk2 == 0)
+                        throw new ArgumentException("luku2 ei saa olla nolla!");
+                    return lk1 / lk2;
+                case 5:
+                    return lk1 % lk2;
+                default:
+                    throw new ArgumentException("Ei käypä vaihtoehto!");
+            }
+        }
+
+        static float KysyLuku(string teksti)
+        {
+            Console.WriteLine(teksti);
+            return Convert.ToSingle(Console.ReadLine());
         }
     }
 }
