be188b9df4040c048010272e1dcf4804be89fb45
L5047 <L5047@student.jamk.fi>
2017-11-06 13:04:28 +0200

    Harj 18 done

--- TTZC0200/Harj18/Program.cs
+++ TTZC0200/Harj18/Program.cs
@@ -23,6 +23,30 @@ namespace Harj18
     {
         static void Main(string[] args)
         {
+            Koira[] Koirat = new Koira[5];
+            for (var i = 0; i < Koirat.Length; i++)
+            {
+                Console.WriteLine("Anna koiran nimi");
+                string nimi = Console.ReadLine();
+
+                Console.WriteLine("Anna koiran ikä");
+                int ika = Convert.ToInt32(Console.ReadLine());
+
+                Koirat[i] = new Koira() { Nimi = nimi, Ika = ika };
+            }
+
+            var palautus = Koirat.OrderBy(x => x.Nimi);
+            foreach (var k in palautus)
+            {
+                Console.WriteLine($"{k.Nimi} | {k.Ika}");
+            }
+            Console.ReadKey();
         }
+    }
+
+    public class Koira
+    {
+        public string Nimi { get; set; }
+        public int Ika { get; set; }
     }
 }
